878cd6e97accb13e32e23b39f242bc588159d5d0
NishantAS <nishantas4043@gamil.com>
2022-04-17 15:30:16 +0530

    version alpha

--- GameBot.py
+++ GameBot.py
@@ -1,4 +1,27 @@
 import discord
-import cv2
+import os
 
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Hello World')
+
+@client.event
+async def on_message(message):
+    if message.author == client.users:
+        return
+
+    msg = message.content
+    if msg.startswith('Hi'):
+        await message.channel.send("Hello")
+    elif msg == "CheckBooyah":
+        await message.channel.send("Works")
+        if message.embeds is not discord.Embed.Empty:
+            await message.channel.send("Has embeds")
+            print(message.embeds.image.url)
+        else:
+            await message.channel.send("Has no embeds")
+
+
+client.run("OTY0NTMyNDI5NDI1NjM5NDc1.YlmA0w.9wJJTF9Aqt7QCXsEANisJHGG7pM")
