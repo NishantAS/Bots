69c1b51a1083f570a0e15dfe33f5c0ed85c1eae2
NishantAS <nishantas4043@gamil.com>
2022-04-17 18:06:16 +0530

    mm

--- GameBot.py
+++ GameBot.py
@@ -1,7 +1,13 @@
 import discord
-import os
+import cv2
+import pytesseract
 
 client = discord.Client()
+
+def somefunc(filename):
+    img = cv2.imread(filename)
+    pytesseract.image_to_string(img, lang= 'en')
+    
 
 @client.event
 async def on_ready():
@@ -17,11 +23,29 @@ async def on_message(message):
         await message.channel.send("Hello")
     elif msg == "CheckBooyah":
         await message.channel.send("Works")
-        if message.embeds is not discord.Embed.Empty:
-            await message.channel.send("Has embeds")
-            print(message.embeds.image.url)
-        else:
-            await message.channel.send("Has no embeds")
+        try:
+            for attachment in message.attachments:
+                filename = attachment.filename
+                with open("text.txt","r") as f:
+                    content = f.readlines()
+
+                for i in content:
+                    if i == filename:
+                        await message.channel.send("Reuse")
+                        break
+                else:
+                    content.append(filename + '\n')                       
+                    await attachment.save(filename)
+                    somefunc(filename)
+                    print(attachment.content_type, attachment.url, filename)
+                    with open("text.txt","w") as f:
+                        f.writelines(content)
+            # img = cv2.imread(url)
+            # string = pytesseract.image_to_string(img, lang= 'en')
+            # print(string)
+
+        except AttributeError:
+            await message.channel.send("Has no Image")
 
 
 client.run("OTY0NTMyNDI5NDI1NjM5NDc1.YlmA0w.9wJJTF9Aqt7QCXsEANisJHGG7pM")
